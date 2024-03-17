7bec58e96891f34edc2298bc30c88b5ea4f12683
Mlie <emipa606@users.noreply.github.com>
2024-03-17 10:53:22 +0100

    Mod updated for 1.5 and passed autotests

--- Source/DesignatorPatch_1.2/DesignatorCostHarmony.cs
+++ Source/DesignatorPatch_1.2/DesignatorCostHarmony.cs
@@ -1,5 +1,4 @@
 using HarmonyLib;
-using RimWorld;
 using Verse;
 
 namespace DesignatorPatch;
@@ -12,27 +11,6 @@ internal class DesignatorCostHarmony
         if (!Harmony.HasAnyPatches("Mlie.DesignatorCost"))
         {
             new Harmony("Mlie.DesignatorCost").PatchAll();
-        }
-    }
-
-    [HarmonyPatch(typeof(Designator_Dropdown), "GetDesignatorCost")]
-    public static class DesignatorDropdownCostPatch
-    {
-        public static bool Prefix(ref ThingDef __result, Designator des)
-        {
-            if (des is not Designator_Place designator_Place)
-            {
-                return true;
-            }
-
-            var placingDef = designator_Place.PlacingDef;
-            if (placingDef.costList != null)
-            {
-                return true;
-            }
-
-            __result = null;
-            return false;
         }
     }
 }
